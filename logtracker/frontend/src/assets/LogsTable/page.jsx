85242d4f3c0a51330659d0489d2f55254a5a824e
Gurnoor Singh <gurnoor1523@gmail.com>
2024-09-28 18:32:50 +0530

    f

--- logtracker/frontend/src/assets/LogsTable/page.jsx
+++ logtracker/frontend/src/assets/LogsTable/page.jsx
@@ -3,21 +3,29 @@ import { DataTable } from "./data-table"
 import logsData from "../rawData"
 import axios from "axios"
 import { getLogsAPI } from "@/services/apis"
+import { response } from "express"
 
 
 export default  function DemoPage() {
 
-  const getLogsHandler = async () => {
-    try {
-      const response = await axios.get(getLogsAPI)
-    } catch (error) {
-      
-    }
-  }
+  const [logData, setlogData] = useState([])
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(getLogsAPI)
+        setlogData(response.data.data)
+      } catch (error) {
+        console.log("error -> ", error);
+        
+      }
+    })();
+  }, [])
+  
+ 
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={logsData} />
+      <DataTable columns={columns} data={logData} />
     </div>
   )
 }
